032fb3f0be47451f402d8672fd23f0ea948fa7e2
RayyanAlbadri023 <rayyanalbadri023@gmail.com>
2025-12-15 12:54:56 +0400

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -16,7 +16,7 @@ const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Welcome />} />
+        <Route path="/" element={<welcome />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/home" element={<Home />} />
